0d460ee77c638bda41ef50d840392ff0471371c0
Ronit Sinha <ronittheawesome@gmail.com>
2018-06-25 12:07:08 -0400

    Cleaning up/load lag accounting

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests
 import cv2
 import numpy as np
 import time
@@ -10,47 +9,6 @@ from datetime import datetime
 
 from kalman_filter import KalmanFilter
 from tracker import Tracker
-
-
-'''
-	
-	NO LONGER USED: see end of README
-
-'''
-# def new_chunk_async (BASE_URL, chunk_dict):
-# 	''' 
-# 	TODO: optimize chunklist loading
-# 	'''
-
-# 	start_time = datetime.utcnow()
-
-# 	r = requests.get (BASE_URL + 'media_' + str(int(chunk_dict['mediaSequence']) + chunk_dict['current_chunk'] + 1) + '.ts')
-
-# 	if r.status_code == 404:
-
-# 		playlist = requests.get (BASE_URL + 'playlist.m3u8')
-
-# 		chunk_dict['chunklistCode'] = playlist.text.splitlines()[3].replace ('chunklist_', '').replace('.m3u8', '')
-
-# 		chunklist = requests.get (BASE_URL + 'chunklist_' + chunk_dict['chunklistCode'] + '.m3u8')
-
-# 		old_mediaSequence = int(chunk_dict['mediaSequence'])
-
-# 		chunk_dict['mediaSequence'] = chunklist.text.splitlines()[3].replace('#EXT-X-MEDIA-SEQUENCE:', '')
-
-# 		offset = (old_mediaSequence + chunk_dict['current_chunk'] + 1) - int(chunk_dict['mediaSequence'])
-
-# 		chunk_dict['current_chunk'] = 0
-# 		# print ('NEW MEDIA SEQUENCE: %s' % (chunk_dict['mediaSequence'], ) )
-# 		new_r = requests.get (BASE_URL + 'media_' + chunk_dict['chunklistCode'] + '_' + str(int(chunk_dict['mediaSequence']) + offset) + '.ts')
-
-# 		open('media_' + chunk_dict['chunklistCode'] + '_' + str(int(chunk_dict['mediaSequence'])) + '.ts', 'wb').write(new_r.content)
-
-# 	else:
-# 		open('media_' + chunk_dict['chunklistCode'] + '_' + str(int(chunk_dict['mediaSequence']) + chunk_dict['current_chunk'] + 1) + '.ts', 'wb').write(r.content)
-
-# 	chunk_dict['load_lag'] = (datetime.utcnow() - start_time).total_seconds()
-# 	print ('LOAD LAG ON SET: %s' % chunk_dict['load_lag'])
 
 
 
@@ -70,28 +28,6 @@ if __name__ == '__main__':
 	'''
 	HIGHWAY_SPEED_LIMIT = 65
 
-
-	playlist = requests.get (BASE_URL + 'playlist.m3u8')
-
-	print ('Got playlist!')
-
-	manager = mpr.Manager()
-
-	# A dictionary is used here because these values where orignally also used asynchronously
-	# This will most likely be changed in the near future
-
-	chunk_dict = manager.dict()
-
-	chunk_dict['chunklistCode'] = playlist.text.splitlines()[3].replace ('chunklist_', '').replace('.m3u8', '')
-
-	chunklist = requests.get (BASE_URL + 'chunklist_' + chunk_dict['chunklistCode'] + '.m3u8')
-
-	chunk_dict['mediaSequence'] = chunklist.text.splitlines()[3].replace('#EXT-X-MEDIA-SEQUENCE:', '')
-
-	chunk_dict['current_chunk'] = 0
-
-	chunk_dict['load_lag'] = 0
-
 	# Initial background subtractor and text font
 	fgbg = cv2.createBackgroundSubtractorMOG2()
 	font = cv2.FONT_HERSHEY_PLAIN
@@ -107,7 +43,7 @@ if __name__ == '__main__':
 	blob_min_width_near = 18
 	blob_min_height_near = 18
 
-	load_lag = 0
+	frame_start_time = None
 
 	# Create object tracker
 	tracker = Tracker(80, 3, 2, 1)
@@ -115,126 +51,101 @@ if __name__ == '__main__':
 	# Capture livestream
 	cap = cv2.VideoCapture (BASE_URL + 'playlist.m3u8')
 
-	# p = mpr.Process(target=new_chunk_async, args=(BASE_URL, chunk_dict))
-	# p.start()
-
 	while True:
 		centers = []
+		frame_start_time = datetime.utcnow()
 		ret, frame = cap.read()
 
 		orig_frame = copy.copy(frame)
 
-		if not ret:
-			'''
-	
-				NO LONGER USED: see end of README
-
-			'''
-
-			# chunk is finished playing; load next one
-			# p.join()
-			# print ('LOAD LAG ON LOAD: %s' % chunk_dict['load_lag'])
-			# load_lag = chunk_dict['load_lag']
-
-			# cap = cv2.VideoCapture ('media_' + chunk_dict['chunklistCode'] + '_' + str(int(chunk_dict['mediaSequence']) + chunk_dict['current_chunk']) + '.ts')
-			# try:
-			# 	os.remove('media_' + chunk_dict['chunklistCode'] + '_' + str(int(chunk_dict['mediaSequence']) + chunk_dict['current_chunk']-1) + '.ts')
-			# except FileNotFoundError:
-			# 	pass
-
-			# chunk_dict['current_chunk'] += 1
-			# p = mpr.Process(target=new_chunk_async, args=(BASE_URL, chunk_dict))
-			# p.start()
-
-			pass
-		else:
-			#  Draw line used for speed detection
-			cv2.line(frame,(0, Y_THRESH),(640, Y_THRESH),(255,0,0),2)
+		#  Draw line used for speed detection
+		cv2.line(frame,(0, Y_THRESH),(640, Y_THRESH),(255,0,0),2)
 
 
-			# Convert frame to grayscale and perform background subtraction
-			gray = cv2.cvtColor (frame, cv2.COLOR_BGR2GRAY)
-			fgmask = fgbg.apply (gray)
+		# Convert frame to grayscale and perform background subtraction
+		gray = cv2.cvtColor (frame, cv2.COLOR_BGR2GRAY)
+		fgmask = fgbg.apply (gray)
 
-			# Perform some Morphological operations to remove noise
-			kernel = np.ones((4,4),np.uint8)
-			kernel_dilate = np.ones((5,5),np.uint8)
-			opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-			dilation = cv2.morphologyEx(opening, cv2.MORPH_OPEN, kernel_dilate)
+		# Perform some Morphological operations to remove noise
+		kernel = np.ones((4,4),np.uint8)
+		kernel_dilate = np.ones((5,5),np.uint8)
+		opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+		dilation = cv2.morphologyEx(opening, cv2.MORPH_OPEN, kernel_dilate)
 
-			_, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		_, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-			# Find centers of all detected objects
-			for cnt in contours:
-				x, y, w, h = cv2.boundingRect(cnt)
+		# Find centers of all detected objects
+		for cnt in contours:
+			x, y, w, h = cv2.boundingRect(cnt)
 
-				if y > Y_THRESH:
-					if w >= blob_min_width_near and h >= blob_min_height_near:
-						center = np.array ([[x+w/2], [y+h/2]])
-						centers.append(np.round(center))
+			if y > Y_THRESH:
+				if w >= blob_min_width_near and h >= blob_min_height_near:
+					center = np.array ([[x+w/2], [y+h/2]])
+					centers.append(np.round(center))
 
-						cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-				else:
-					if w >= blob_min_width_far and h >= blob_min_height_far:
-						center = np.array ([[x+w/2], [y+h/2]])
-						centers.append(np.round(center))
+					cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+			else:
+				if w >= blob_min_width_far and h >= blob_min_height_far:
+					center = np.array ([[x+w/2], [y+h/2]])
+					centers.append(np.round(center))
 
-						cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+					cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-			if centers:
-				tracker.update(centers)
+		if centers:
+			tracker.update(centers)
 
-				for vehicle in tracker.tracks:
-					if len(vehicle.trace) > 1:
-						for j in range(len(vehicle.trace)-1):
-	                        # Draw trace line
-							x1 = vehicle.trace[j][0][0]
-							y1 = vehicle.trace[j][1][0]
-							x2 = vehicle.trace[j+1][0][0]
-							y2 = vehicle.trace[j+1][1][0]
+			for vehicle in tracker.tracks:
+				if len(vehicle.trace) > 1:
+					for j in range(len(vehicle.trace)-1):
+                        # Draw trace line
+						x1 = vehicle.trace[j][0][0]
+						y1 = vehicle.trace[j][1][0]
+						x2 = vehicle.trace[j+1][0][0]
+						y2 = vehicle.trace[j+1][1][0]
 
-							cv2.line(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 255), 2)
+						cv2.line(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 255), 2)
 
-						try:
-							'''
-								TODO: account for load lag
-							'''
+					try:
+						'''
+							TODO: account for load lag
+						'''
 
-							trace_i = len(vehicle.trace) - 1
+						trace_i = len(vehicle.trace) - 1
 
-							trace_x = vehicle.trace[trace_i][0][0]
-							trace_y = vehicle.trace[trace_i][1][0]
+						trace_x = vehicle.trace[trace_i][0][0]
+						trace_y = vehicle.trace[trace_i][1][0]
 
-							# Check if tracked object has reached the speed detection line
-							if trace_y <= Y_THRESH + 5 and trace_y >= Y_THRESH - 5 and not vehicle.passed:
-								cv2.putText(frame, 'I PASSED!', (int(trace_x), int(trace_y)), font, 1, (0, 255, 255), 1, cv2.LINE_AA)
-								vehicle.passed = True
-								time_dur = (datetime.utcnow() - vehicle.start_time).total_seconds() - load_lag
-								time_dur /= 60
-								time_dur /= 60
+						# Check if tracked object has reached the speed detection line
+						if trace_y <= Y_THRESH + 5 and trace_y >= Y_THRESH - 5 and not vehicle.passed:
+							cv2.putText(frame, 'I PASSED!', (int(trace_x), int(trace_y)), font, 1, (0, 255, 255), 1, cv2.LINE_AA)
+							vehicle.passed = True
 
-								
-								vehicle.mph = ROAD_DIST_MILES / time_dur
+							load_lag = (datetime.utcnow() - frame_start_time).total_seconds()
 
-								# If calculated speed exceeds speed limit, save an image of speeding car
-								if vehicle.mph > HIGHWAY_SPEED_LIMIT:
-									print ('UH OH, SPEEDING!')
-									cv2.circle(orig_frame, (int(trace_x), int(trace_y)), 20, (0, 0, 255), 2)
-									cv2.putText(orig_frame, 'MPH: %s' % int(vehicle.mph), (int(trace_x), int(trace_y)), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
-									cv2.imwrite('speeding_%s.png' % vehicle.track_id, orig_frame)
-									print ('FILE SAVED!')
+							time_dur = (datetime.utcnow() - vehicle.start_time).total_seconds() - load_lag
+							time_dur /= 60
+							time_dur /= 60
 
-						
-							if vehicle.passed:
-								# Display speed if available
-								cv2.putText(frame, 'MPH: %s' % int(vehicle.mph), (int(trace_x), int(trace_y)), font, 1, (0, 255, 255), 1, cv2.LINE_AA)
-							else:
-								# Otherwise, just show tracking id
-								cv2.putText(frame, 'ID: '+ str(vehicle.track_id), (int(trace_x), int(trace_y)), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
-						except:
-							pass
+							
+							vehicle.mph = ROAD_DIST_MILES / time_dur
 
-			chunk_dict['load_lag'] = 0
+							# If calculated speed exceeds speed limit, save an image of speeding car
+							if vehicle.mph > HIGHWAY_SPEED_LIMIT:
+								print ('UH OH, SPEEDING!')
+								cv2.circle(orig_frame, (int(trace_x), int(trace_y)), 20, (0, 0, 255), 2)
+								cv2.putText(orig_frame, 'MPH: %s' % int(vehicle.mph), (int(trace_x), int(trace_y)), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
+								cv2.imwrite('speeding_%s.png' % vehicle.track_id, orig_frame)
+								print ('FILE SAVED!')
+
+					
+						if vehicle.passed:
+							# Display speed if available
+							cv2.putText(frame, 'MPH: %s' % int(vehicle.mph), (int(trace_x), int(trace_y)), font, 1, (0, 255, 255), 1, cv2.LINE_AA)
+						else:
+							# Otherwise, just show tracking id
+							cv2.putText(frame, 'ID: '+ str(vehicle.track_id), (int(trace_x), int(trace_y)), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+					except:
+						pass
 
 
 		# Display all images
@@ -252,9 +163,6 @@ if __name__ == '__main__':
 	# Clean up
 	cap.release()
 	cv2.destroyAllWindows()
-
-	# catch any running processes
-	# p.join()
 
 	# remove all speeding_*.png images created in runtime
 	for file in glob.glob('speeding_*.png'):
